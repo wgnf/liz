c55f92722913dd8f74fa5e67e7e113590898758d
Martin Wagenführ <martin@wgnf.de>
2021-09-30 16:23:27 +0200

    Add TODO List on the things that need to be done

--- src/DotnetNugetLicenses.Core/ExtractLicenses.cs
+++ src/DotnetNugetLicenses.Core/ExtractLicenses.cs
@@ -1,6 +1,5 @@
 ﻿using Ardalis.GuardClauses;
 using DotnetNugetLicenses.Core.Contracts;
-using System;
 
 namespace DotnetNugetLicenses.Core
 {
@@ -9,8 +8,29 @@ namespace DotnetNugetLicenses.Core
 		public void Extract(ExtractSettings settings)
 		{
 			Guard.Against.Null(settings, nameof(settings));
-
-			throw new NotImplementedException();
-		}
+            
+            /*
+             * -- TODO --
+             * 
+             * get projects -> sln: Parse + Select csproj/fsproj | csproj: take csproj
+             * filter projects (by name with Regex)
+             *
+             * get references (including/excluding transitive)
+             * include manual packages
+             * filter references (by name/version with Regex)
+             *
+             * get information for references (however this works...) including raw text if requested
+             * include manual License-URL to License Mappings
+             *
+             * validate references with their licenses (with the provided allowed licenses)
+             *
+             *
+             * in Tool (according to parameters):
+             * - print Package-Name + License-Type + License-URL
+             * - export License-Texts (package-name as the File-Name) into a specified directory
+             * - export Package + License-Details in a JSON-Format (including/excluding Raw Text)
+             * 
+             */
+        }
 	}
 }
