9dffb3af131de2fd1d509a30e7ba107d0f9142e6
Martin Wagenführ <martin@wgnf.de>
2021-12-19 15:27:07 +0100

    feat: add model for license information

---
+++ src/Core/Liz.Core/License/LicenseInformation.cs
@@ -0,0 +1,7 @@
+﻿using Liz.Core.PackageReferences;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Liz.Core.License;
+
+[ExcludeFromCodeCoverage]
+internal sealed record LicenseInformation(string LicenseType, string RawLicenseText, PackageReference Package);
