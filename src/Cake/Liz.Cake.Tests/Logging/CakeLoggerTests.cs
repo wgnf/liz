12d7b87b132e674ca252093cfdb19c6218012202
Martin Wagenführ <martin.wagenfuehr@schoenhofer.de>
2022-02-16 14:57:03 +0100

    feat: implement cake aliases
    Closes #17

---
+++ src/Cake/Liz.Cake.Tests/Logging/CakeLoggerTests.cs
@@ -0,0 +1,43 @@
+﻿using ArrangeContext.Moq;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Liz.Cake.Logging;
+using Moq;
+using Xunit;
+using LogLevel = Liz.Core.Logging.Contracts.LogLevel;
+using CakeLogLevel = Cake.Core.Diagnostics.LogLevel;
+
+namespace Liz.Cake.Tests.Logging;
+
+public class CakeLoggerTests
+{
+    [Theory]
+    [InlineData(LogLevel.Trace, Verbosity.Diagnostic, CakeLogLevel.Debug)]
+    [InlineData(LogLevel.Debug, Verbosity.Verbose, CakeLogLevel.Verbose)]
+    [InlineData(LogLevel.Information, Verbosity.Normal, CakeLogLevel.Information)]
+    [InlineData(LogLevel.Warning, Verbosity.Normal, CakeLogLevel.Warning)]
+    [InlineData(LogLevel.Error, Verbosity.Normal, CakeLogLevel.Error)]
+    [InlineData(LogLevel.Critical, Verbosity.Normal, CakeLogLevel.Error)]
+    [InlineData(LogLevel.None, Verbosity.Quiet, CakeLogLevel.Information)]
+    public void CakeLogger_Logs_With_Correctly_Mapped_Cake_Values(
+        LogLevel lizLogLevel,
+        Verbosity expectedCakeVerbosity,
+        CakeLogLevel expectedCakeLogLevel)
+    {
+        var context = ArrangeContext<CakeLogger>.Create();
+        var sut = context.Build();
+
+        var cakeLogMock = new Mock<ICakeLog>();
+        context
+            .For<ICakeContext>()
+            .SetupGet(cakeContext => cakeContext.Log)
+            .Returns(cakeLogMock.Object);
+
+        const string message = "something";
+        
+        sut.Log(lizLogLevel, message);
+        
+        cakeLogMock.Verify(cakeLog => 
+            cakeLog.Write(expectedCakeVerbosity, expectedCakeLogLevel, message));
+    }
+}
---
+++ src/Cake/Liz.Cake/LizAliases.cs
@@ -0,0 +1,89 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+using Liz.Cake.Logging;
+using Liz.Core;
+using Liz.Core.Extract.Contracts;
+using Liz.Core.Logging.Contracts;
+using Liz.Core.PackageReferences.Contracts.Models;
+using Liz.Core.Settings;
+using System.Diagnostics.CodeAnalysis;
+using Path = Cake.Core.IO.Path;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+
+namespace Liz.Cake;
+
+/// <summary>
+///     Aliases for the Liz-Tool to extract licenses
+/// </summary>
+[ExcludeFromCodeCoverage] // not sure how to test this and if this even makes sense (in the future for integration-test, yes)
+[CakeAliasCategory("Licenses")]
+public static class LizAliases
+{
+    /// <summary>
+    ///     Extract licenses for the provided <paramref name="targetFile"/> with the default settings
+    /// </summary>
+    /// <param name="context">The cake context to execute on</param>
+    /// <param name="targetFile">The target-file to analyze. This can be a .csproj,.fsproj,.sln</param>
+    /// <returns>The package-references enriched with the license-information that have been gathered</returns>
+    /// <exception cref="ArgumentNullException">Thrown when any of the parameters is null</exception>
+    [CakeMethodAlias]
+    [CakeAliasCategory("ExtractLicenses")]
+    public async static Task<IEnumerable<PackageReference>> ExtractLicensesAsync(this ICakeContext context, FilePath targetFile)
+    {
+        if (context == null) throw new ArgumentNullException(nameof(context));
+        if (targetFile == null) throw new ArgumentNullException(nameof(targetFile));
+
+        var settings = new ExtractLicensesToolSettings();
+        var packageReferences = await ExtractLicensesAsync(context, targetFile, settings);
+        return packageReferences;
+    }
+
+    /// <summary>
+    ///     Extract licenses for the provided <paramref name="targetFile"/> with the provided settings
+    /// </summary>
+    /// <param name="context">The cake context to execute on</param>
+    /// <param name="targetFile">The target-file to analyze. This can be a .csproj,.fsproj,.sln</param>
+    /// <param name="settings">The settings for the execution</param>
+    /// <returns>The package-references enriched with the license-information that have been gathered</returns>
+    /// <exception cref="ArgumentNullException">Thrown when any of the parameters is null</exception>
+    [CakeMethodAlias]
+    [CakeAliasCategory("ExtractLicenses")]
+    public async static Task<IEnumerable<PackageReference>> ExtractLicensesAsync(
+        this ICakeContext context, 
+        FilePath targetFile,
+        ExtractLicensesToolSettings settings)
+    {
+        if (context == null) throw new ArgumentNullException(nameof(context));
+        if (targetFile == null) throw new ArgumentNullException(nameof(targetFile));
+        if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+        var extractLicenses = GetExtractLicensesInstance(context, targetFile, settings);
+
+        var packageReferences = await extractLicenses.ExtractAsync();
+        return packageReferences;
+    }
+
+    private static IExtractLicenses GetExtractLicensesInstance(
+        ICakeContext context,
+        Path targetFile, 
+        ExtractLicensesToolSettings settings)
+    {
+        var loggerProvider = new CakeLoggerProvider(context);
+        var extractLicensesSettings = new ExtractLicensesSettings(targetFile.FullPath)
+        {
+            IncludeTransitiveDependencies = settings.IncludeTransitiveDependencies,
+            // just use the lowest one, because the log level comes from cake itself
+            LogLevel = LogLevel.Trace,
+            SuppressPrintDetails = settings.SuppressPrintDetails,
+            SuppressPrintIssues = settings.SuppressPrintIssues
+        };
+
+        var extractLicensesFactory = new ExtractLicensesFactory();
+        var extractLicenses = extractLicensesFactory.Create(extractLicensesSettings, loggerProvider);
+        return extractLicenses;
+    }
+}
