6377326f94a5740c4db621002a0d1f7f3104592f
Martin Wagenführ <martin@wgnf.de>
2022-03-25 23:25:45 +0100

    feat: add some widely used license type definitions

--- src/Core/Liz.Core.Tests/License/Sources/LicenseType/LicenseTypeDefinitionProviderTests.cs
+++ src/Core/Liz.Core.Tests/License/Sources/LicenseType/LicenseTypeDefinitionProviderTests.cs
@@ -22,6 +22,13 @@ public class LicenseTypeDefinitionProviderTests
 
     [Theory]
     [InlineData("MIT")]
+    [InlineData("GPL-3.0")]
+    [InlineData("Apache-2.0")]
+    [InlineData("BSD-2-Clause")]
+    [InlineData("BSD-3-Clause")]
+    [InlineData("LGPL-3.0")]
+    [InlineData("MS-PL")]
+    [InlineData("Unlicense")]
     public void Provides_Definitions_For_Widely_Used_License_Types(string licenseType)
     {
         var context = ArrangeContext<LicenseTypeDefinitionProvider>.Create();
