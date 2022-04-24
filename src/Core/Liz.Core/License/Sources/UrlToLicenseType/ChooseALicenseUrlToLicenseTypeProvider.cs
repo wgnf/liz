34cf73678fc874c9c1634ba6ab888db9c90d7596
Martin Wagenführ <martin@wgnf.de>
2022-04-24 15:56:08 +0200

    feat: add http copies of choose-a-license urls

--- src/Core/Liz.Core/License/Sources/UrlToLicenseType/ChooseALicenseUrlToLicenseTypeProvider.cs
+++ src/Core/Liz.Core/License/Sources/UrlToLicenseType/ChooseALicenseUrlToLicenseTypeProvider.cs
@@ -18,7 +18,17 @@ internal sealed class ChooseALicenseUrlToLicenseTypeProvider : IUrlToLicenseType
             { "https://choosealicense.com/licenses/apache-2.0/", "Apache-2.0" },
             { "https://choosealicense.com/licenses/mit/", "MIT" },
             { "https://choosealicense.com/licenses/bsl-1.0/", "BSL-1.0" },
-            { "https://choosealicense.com/licenses/unlicense/", "Unlicense" }
+            { "https://choosealicense.com/licenses/unlicense/", "Unlicense" },
+            
+            // an exact copy of the links above, just using http instead of http (as it seems some are still using http)
+            { "http://choosealicense.com/licenses/agpl-3.0/", "AGPL-3.0" },
+            { "http://choosealicense.com/licenses/gpl-3.0/", "GPL-3.0" },
+            { "http://choosealicense.com/licenses/lgpl-3.0/", "LGPL-3.0" },
+            { "http://choosealicense.com/licenses/mpl-2.0/", "MPL-2.0" },
+            { "http://choosealicense.com/licenses/apache-2.0/", "Apache-2.0" },
+            { "http://choosealicense.com/licenses/mit/", "MIT" },
+            { "http://choosealicense.com/licenses/bsl-1.0/", "BSL-1.0" },
+            { "http://choosealicense.com/licenses/unlicense/", "Unlicense" }
         };
     }
 }
