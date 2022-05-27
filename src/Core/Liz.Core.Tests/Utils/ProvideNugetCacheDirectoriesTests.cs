2e3a4bc74885dbfb212bb4a1788055efb707bb99
Martin Wagenführ <martin.wagenfuehr@schoenhofer.de>
2022-05-27 21:00:26 +0200

    feat: add a provider that provides nuget caches

---
+++ src/Core/Liz.Core.Tests/Utils/ProvideNugetCacheDirectoriesTests.cs
@@ -0,0 +1,39 @@
+﻿using ArrangeContext.Moq;
+using FluentAssertions;
+using Liz.Core.CliTool.Contracts;
+using Liz.Core.Utils;
+using Moq;
+using Xunit;
+
+namespace Liz.Core.Tests.Utils;
+
+public class ProvideNugetCacheDirectoriesTests
+{
+    [Fact]
+    public async Task Provides_Cache_Directories()
+    {
+        var context = ArrangeContext<ProvideNugetCacheDirectories>.Create();
+        var sut = context.Build();
+
+        const string expectedDirectory = @"C:\Users\some-user\.nuget\packages\";
+        const string exampleResult = $@"global-packages: {expectedDirectory}";
+        context
+            .For<ICliToolExecutor>()
+            .Setup(cliToolExecutor => cliToolExecutor.ExecuteWithResultAsync(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(Task.FromResult(exampleResult));
+
+        var result = await sut.GetAsync();
+
+        result
+            .Should()
+            .OnlyContain(directory => directory == expectedDirectory);
+
+        // getting the directory should be cached, so a second call should just return the cache
+        _ = await sut.GetAsync();
+        
+        context
+            .For<ICliToolExecutor>()
+            .Verify(cliToolExecutor => cliToolExecutor.ExecuteWithResultAsync(It.IsAny<string>(), It.IsAny<string>()),
+                Times.Once);
+    }
+}
